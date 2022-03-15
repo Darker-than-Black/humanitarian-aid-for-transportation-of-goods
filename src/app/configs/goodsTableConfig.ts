e2c37f4deeddc7c125960d4ca43fb8ae9a0afc77
Dmytro Rybachek <Puss.in.Boots@i.ua>
2022-03-15 11:33:45 +0200

    added "sender" column

--- src/app/configs/goodsTableConfig.ts
+++ src/app/configs/goodsTableConfig.ts
@@ -4,6 +4,7 @@ export const GOODS_TABLE_CONFIG: TableColumnConfig[] = [
   { header: '№ п/п', field: 'id' },
   { header: 'Ідентифікатор партії', field: 'name' },
   { header: 'Отримувач', field: 'recipient' },
+  { header: 'Відправник', field: 'sender' },
   { header: 'Об’єм партії', field: 'volume' },
   { header: 'Температурний контроль', field: 'temperature' },
   { header: 'Статус', field: 'status_name' },
